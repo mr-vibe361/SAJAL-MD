a7b60ee4e3c1de2bb987db0b749efd17b5d05642
mr-vibe361 <dustusajal878@gmail.com>
2025-09-07 11:28:07 +0530

    Update confing.js

--- confing.js
+++ confing.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
     ownerNumber: process.env.OWNER_NUMBER + '@s.whatsapp.net',
     openaiApiKey: process.env.OPENAI_API_KEY,
-    prefix: process.env.PREFIX || '.',
+    prefix: process.env.PREFIX || '!',
     sessionName: process.env.SESSION_NAME || 'SAJAL_MD',
     timezone: process.env.TIMEZONE || 'Asia/Mumbai',
     maxFileSize: parseInt(process.env.MAX_FILE_SIZE) || 50
